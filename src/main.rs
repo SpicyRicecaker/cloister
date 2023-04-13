0a54ecbe79d7a04f74bcdf2e987fc375c7d742d7
SpicyRicecaker <awesomenessisgolden@gmail.com>
2023-04-12 18:06:36 -0700

    use deflate. added pretty logs.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    fs::{self, read_dir, DirEntry, File, FileType},
+    fs::File,
     io::Seek,
     io::{Read, Write},
     path::{Path, PathBuf},
@@ -8,8 +8,8 @@ use std::{
 use anyhow::{bail, Context, Error};
 use clap::{Parser, Subcommand};
 use git2::{Repository, RepositoryOpenFlags, StatusOptions};
-use log::info;
-use zip::{result::ZipError, write::FileOptions, ZipWriter};
+use log::{info, LevelFilter};
+use zip::{write::FileOptions, ZipWriter};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -25,15 +25,16 @@ enum Commands {
     Zip { path: String },
 }
 
+#[allow(dead_code)]
 enum Subcommands {
     Add,
 }
 
 fn main() -> Result<(), Error> {
-    env_logger::init();
-    // user must supply a path to a repository, as well as the relative path into that repository.
-    // but that's really annoying. for usability, we should just let the user
-    // specify a path, and recurse up into that repository.
+    env_logger::builder()
+        .filter_level(LevelFilter::Info)
+        .init();
+    // user supplies a path to a repository, as well as the relative subfolder into that repository.
     let cli = Cli::parse();
 
     match &cli.command {
@@ -44,8 +45,6 @@ fn main() -> Result<(), Error> {
             if !path.exists() {
                 bail!("path {path:?} does not exist...");
             }
-
-            // dbg!("f", &path);
 
             git(path)?;
         }
@@ -82,21 +81,16 @@ fn git(src_dir: &Path) -> Result<(), Error> {
     let c: Vec<PathBuf> = statuses
         .into_iter()
         .map(|s| PathBuf::from(s.path().unwrap()))
-        // .map(|p| workdir.join(p))
         .collect();
 
-    // dbg!(&c);
-    dbg!(src_dir, &zip_file);
+    info!("begining to compress {src_dir:?} into {:?}", &zip_file);
 
     zip_dir(
         c,
-        src_dir
-            .to_str()
-            .context("unable to convert")?,
+        src_dir.to_str().context("unable to convert")?,
         zip_file,
-        zip::CompressionMethod::Bzip2,
+        zip::CompressionMethod::Deflated,
     )?;
-    
 
     info!("done!!");
 
@@ -112,169 +106,34 @@ fn zip_dir<T>(
 where
     T: Write + Seek,
 {
-    dbg!(prefix);
+    info!("found git repo at: {prefix:?}");
+
     let mut zip = ZipWriter::new(writer);
     let options = FileOptions::default()
         .compression_method(method)
+        // read, write
         .unix_permissions(0o755);
 
     let mut buffer = Vec::new();
     for path in it {
-        dbg!(&path);
-        // for entry in fs::read_dir(path_current)? {
-        let name = path.strip_prefix(Path::new(prefix)).unwrap();
+        let name = path
+            .strip_prefix(Path::new(prefix))
+            .unwrap()
+            .to_str()
+            .unwrap();
 
-        // Write file or directory explicitly
-        // Some unzip tools unzip files with directory paths correctly, some do not!
-        if path.is_file() {
-            println!("adding file {path:?} as {name:?} ...");
-            // dbg!(&path, &name);
-            #[allow(deprecated)]
-            zip.start_file_from_path(name, options)?;
-            let mut f = File::open(path)?;
-
-            f.read_to_end(&mut buffer)?;
-            zip.write_all(&buffer)?;
-            buffer.clear();
-        } else if !name.as_os_str().is_empty() {
-            unreachable!();
-            // Only if not root! Avoids path spec / warning
-            // and mapname conversion failed error on unzip
-            println!("adding dir {path:?} as {name:?} ...");
-            #[allow(deprecated)]
-            zip.add_directory_from_path(name, options)?;
+        if !path.is_file() {
+            panic!();
         }
-        // }
+
+        // add contents of file into zip
+        info!("adding file {path:?} as {name:?} to {prefix:?}.zip ...");
+        zip.start_file(name, options)?;
+        let mut f = File::open(path)?;
+        f.read_to_end(&mut buffer)?;
+        zip.write_all(&buffer)?;
+        buffer.clear();
     }
     zip.finish()?;
     Result::Ok(())
 }
-
-// // BEGIN EXAMPLE LOL XD
-
-// use std::io::prelude::*;
-// use std::io::{Seek, Write};
-// use std::iter::Iterator;
-// use zip::result::ZipError;
-// use zip::write::FileOptions;
-
-// use std::fs::File;
-// use std::path::Path;
-// use walkdir::{DirEntry, WalkDir};
-
-// fn main() {
-//     std::process::exit(real_main());
-// }
-
-// const METHOD_STORED: Option<zip::CompressionMethod> = Some(zip::CompressionMethod::Stored);
-
-// #[cfg(any(
-//     feature = "deflate",
-//     feature = "deflate-miniz",
-//     feature = "deflate-zlib"
-// ))]
-// const METHOD_DEFLATED: Option<zip::CompressionMethod> = Some(zip::CompressionMethod::Deflated);
-// #[cfg(not(any(
-//     feature = "deflate",
-//     feature = "deflate-miniz",
-//     feature = "deflate-zlib"
-// )))]
-// const METHOD_DEFLATED: Option<zip::CompressionMethod> = None;
-
-// #[cfg(feature = "bzip2")]
-// const METHOD_BZIP2: Option<zip::CompressionMethod> = Some(zip::CompressionMethod::Bzip2);
-// #[cfg(not(feature = "bzip2"))]
-// const METHOD_BZIP2: Option<zip::CompressionMethod> = None;
-
-// #[cfg(feature = "zstd")]
-// const METHOD_ZSTD: Option<zip::CompressionMethod> = Some(zip::CompressionMethod::Zstd);
-// #[cfg(not(feature = "zstd"))]
-// const METHOD_ZSTD: Option<zip::CompressionMethod> = None;
-
-// fn real_main() -> i32 {
-//     let args: Vec<_> = std::env::args().collect();
-//     if args.len() < 3 {
-//         println!(
-//             "Usage: {} <source_directory> <destination_zipfile>",
-//             args[0]
-//         );
-//         return 1;
-//     }
-
-//     let src_dir = &*args[1];
-//     let dst_file = &*args[2];
-//     for &method in [METHOD_STORED, METHOD_DEFLATED, METHOD_BZIP2, METHOD_ZSTD].iter() {
-//         if method.is_none() {
-//             continue;
-//         }
-//         match doit(src_dir, dst_file, method.unwrap()) {
-//             Ok(_) => println!("done: {src_dir} written to {dst_file}"),
-//             Err(e) => println!("Error: {e:?}"),
-//         }
-//     }
-
-//     0
-// }
-
-// fn zip_dir<T>(
-//     it: &mut dyn Iterator<Item = DirEntry>,
-//     prefix: &str,
-//     writer: T,
-//     method: zip::CompressionMethod,
-// ) -> zip::result::ZipResult<()>
-// where
-//     T: Write + Seek,
-// {
-//     let mut zip = zip::ZipWriter::new(writer);
-//     let options = FileOptions::default()
-//         .compression_method(method)
-//         .unix_permissions(0o755);
-
-//     let mut buffer = Vec::new();
-//     for entry in it {
-//         let path = entry.path();
-//         let name = path.strip_prefix(Path::new(prefix)).unwrap();
-
-//         // Write file or directory explicitly
-//         // Some unzip tools unzip files with directory paths correctly, some do not!
-//         if path.is_file() {
-//             println!("adding file {path:?} as {name:?} ...");
-//             #[allow(deprecated)]
-//             zip.start_file_from_path(name, options)?;
-//             let mut f = File::open(path)?;
-
-//             f.read_to_end(&mut buffer)?;
-//             zip.write_all(&buffer)?;
-//             buffer.clear();
-//         } else if !name.as_os_str().is_empty() {
-//             // Only if not root! Avoids path spec / warning
-//             // and mapname conversion failed error on unzip
-//             println!("adding dir {path:?} as {name:?} ...");
-//             #[allow(deprecated)]
-//             zip.add_directory_from_path(name, options)?;
-//         }
-//     }
-//     zip.finish()?;
-//     Result::Ok(())
-// }
-
-// fn doit(
-//     src_dir: &str,
-//     dst_file: &str,
-//     method: zip::CompressionMethod,
-// ) -> zip::result::ZipResult<()> {
-//     if !Path::new(src_dir).is_dir() {
-//         return Err(ZipError::FileNotFound);
-//     }
-
-//     let path = Path::new(dst_file);
-//     let file = File::create(path).unwrap();
-
-//     let walkdir = WalkDir::new(src_dir);
-//     let it = walkdir.into_iter();
-
-//     dbg!(src_dir, &file);
-//     zip_dir(&mut it.filter_map(|e| e.ok()), src_dir, file, method)?;
-
-//     Ok(())
-// }
